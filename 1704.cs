8023ed1f61cfdda717621a9ea0594a6e3f397043
anzar00 <peerzada.anzar@gmail.com>
2024-01-12 22:36:25 +0530

    12-Jan

---
+++ 1704.cs
@@ -0,0 +1,46 @@
+// Determine if String Halves Are Alike
+
+/* You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+ Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). 
+ Notice that s contains uppercase and lowercase letters.
+ Return true if a and b are alike. Otherwise, return false.
+  
+ Example 1:
+ Input: s = "book"
+ Output: true
+ Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+  
+ Example 2:
+ Input: s = "textbook"
+ Output: false
+ Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+ Notice that the vowel o is counted twice.
+ 
+ Constraints:
+ 2 <= s.length <= 1000
+ s.length is even.
+ s consists of uppercase and lowercase letters.
+ */
+
+ public class Solution {
+    public bool HalvesAreAlike(string s) {
+
+        int count = 0;
+        int half = s.Length / 2;
+        string vowels = "aeiouAEIOU";
+        
+        for (int i = 0; i < half; i++) {
+            if (vowels.Contains(s[i])) {
+                count++;
+            }
+        }
+        
+        for (int i = half; i < s.Length; i++) {
+            if (vowels.Contains(s[i])) {
+                count--;
+            }
+        }
+
+        return count == 0;
+    }
+ }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # LeetCodeProblems
+
+Solving LeetCode problems in C#.
+
+| Date | Problem | Difficulty | 
+|------|---------|------------|
+| 12-Jan-24 | [1704](1704.cs) | Easy | 
