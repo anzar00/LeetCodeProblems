ca15d8093b8930059c7f3f91d875087801dd198b
anzar00 <peerzada.anzar@gmail.com>
2024-01-13 23:15:37 +0530

    13-Jan

---
+++ 1347.cs
@@ -0,0 +1,60 @@
+// Minimum Number of steps to make two strings anagram
+
+/*
+
+You are given two strings of the same length s and t. In one step you can choose any character of t and replace it with another character.
+
+Return the minimum number of steps to make t an anagram of s.
+
+An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+
+Example 1:
+
+Input: s = "bab", t = "aba"
+Output: 1
+Explanation: Replace the first 'a' in t with a 'b', t = "bba" which is anagram of s.
+
+Example 2:
+
+Input: s = "leetcode", t = "practice"
+Output: 5
+Explanation: Replace 'p', 'r', 'a', 'i' and 'c' from t with proper characters to make t anagram of s.
+
+Example 3:
+
+Input: s = "anagram", t = "mangaar"
+Output: 0
+Explanation: "anagram" and "mangaar" are anagrams.
+
+Constraints:
+
+    1 <= s.length <= 50000
+    s.length == t.length
+    s and t contain lower-case English letters only.
+
+*/
+
+public class Solution {
+    public int MinSteps(string s, string t) {
+        int[] sCount = new int[26];
+        int[] tCount = new int[26];
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            sCount[s[i] - 'a']++;
+            tCount[t[i] - 'a']++;
+        }
+
+        int steps = 0;
+
+        for(int i = 0; i < 26; i++)
+        {
+            if(sCount[i] > tCount[i])
+            {
+                steps += sCount[i] - tCount[i];
+            }
+        }
+
+        return steps;
+    }
+}
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Solving LeetCode problems in C#.
 
 | Date | Problem | Difficulty | 
 |------|---------|------------|
+| 13-Jan-24 | [1347](1347.cs) | Medium | 
 | 12-Jan-24 | [1704](1704.cs) | Easy | 
 | 11-Jan-24 | [1026](1026.cs) | Medium | 
 | 10-Jan-24 | [2385](2385.cs) | Medium | 
